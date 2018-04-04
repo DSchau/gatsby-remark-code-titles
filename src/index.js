f2b30da6a3bc78ebb014e9bbd125ad97cc011837
Dustin Schau <dustinschau@gmail.com>
2018-04-04 17:57:17 -0500

    feat: initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import visit from 'unist-util-visit';
+import qs from 'query-string';
+
+module.exports = function gatsbyRemarkCodeTitles({ markdownAST }, { className: customClassName }) {
+  visit(markdownAST, 'code', (node, index) => {
+    const [language, query] = node.lang.split(':');
+    const { title } = qs.parse(query);
+    if (!title) {
+      return;
+    }
+
+    const className = ['gatsby-code-title'].concat(customClassName || []);
+
+    const titleNode = {
+      type: 'html',
+      value: `
+<div class="${className.join(' ').trim()}">${title}</div>
+      `.trim()
+    };
+
+    /*
+     * Splice a node back into the Markdown AST with custom title
+     */
+    markdownAST.children.splice(index, 0, titleNode);
+
+    /*
+     * Reset to just the language
+     */
+    node.lang = language;
+  });
+};
