41238d4ee940f3911ca0afe9f257cb18648d85f2
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2018-11-08 23:07:35 +0100

    Add support for copying the code snippet to the clipboard

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import visit from 'unist-util-visit';
 import qs from 'query-string';
 
-module.exports = function gatsbyRemarkCodeTitles({ markdownAST }, { className: customClassName }) {
+module.exports = function gatsbyRemarkCodeTitles(
+  { markdownAST },
+  { className: customClassName, copy = false } = {}
+) {
   visit(markdownAST, 'code', (node, index) => {
     const [language, query] = (node.lang || '').split(':');
     const { title } = qs.parse(query);
@@ -10,12 +13,24 @@ module.exports = function gatsbyRemarkCodeTitles({ markdownAST }, { className: c
     }
 
     const className = ['gatsby-code-title'].concat(customClassName || []);
+    const copyFunction = `(function () { if ('clipboard' in navigator) {navigator.clipboard.writeText('${
+      node.value
+    }').then(function() {alert('Copied to clipboard')}, function() { alert('Error copying code to clipboard')})}})()`;
+
+    const button = `
+      <button type="button" class="gastsby-code-title-copy" alt="Copy to clipboard" onclick="${copyFunction}">
+        Copy
+      </button>
+      `;
 
     const titleNode = {
       type: 'html',
       value: `
-<div class="${className.join(' ').trim()}">${title}</div>
-      `.trim()
+    <div class="${className.join(' ').trim()}">
+      ${title}
+      ${copy ? button : ''}
+    </div>
+      `.trim(),
     };
 
     /*
